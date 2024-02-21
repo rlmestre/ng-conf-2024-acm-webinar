04c29878c693be5c0666e454819933c53b5e02c4
Rafael Mestre <rafaelmestre@outlook.com>
2024-02-21 17:56:00 -0400

    rxjs-interop final state

--- src/treasure-list.component.ts
+++ src/treasure-list.component.ts
@@ -1,57 +1,59 @@
-import { AsyncPipe, NgFor, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
-import {
-    BehaviorSubject, combineLatest, EMPTY, interval, map, merge, of, scan, startWith, Subject, switchMap, throttleTime
-} from 'rxjs';
+import { AsyncPipe, JsonPipe, NgFor, NgIf } from '@angular/common';
+import { Component, model, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { FormsModule } from '@angular/forms';
+import { EMPTY, interval, map, merge, of, scan, Subject, switchMap, tap, throttleTime } from 'rxjs';
 import { TreasureComponent } from  './treasure.component';
 import { treasuresMock } from './treasures.mock';
 
 @Component({
     selector: 'treasure-list',
-    imports: [NgIf, NgFor, AsyncPipe, TreasureComponent],
+    imports: [NgIf, NgFor, AsyncPipe, JsonPipe, FormsModule, TreasureComponent],
     standalone: true,
     template: `
-        <ng-container *ngIf="vm$ | async; let vm">
-            <h1>Discovering Angular 17's Hidden Treasures</h1>
+        <h1>Discovering Angular 17's Hidden Treasures</h1>
+        <main>
             <div class="buttons">
                 <button (click)="getNextArticle()">Get next article</button>
-                <button (click)="toggleStreaming()">{{ vm.streaming ? 'Stop streaming' : 'Stream articles' }}</button>
-                <input type="text" placeholder="Search" />
+                <button (click)="toggleStreaming()">{{ streaming() ? 'Stop streaming' : 'Stream articles' }}</button>
+                <input type="text" placeholder="Search treasure..." [(ngModel)]="searchId" />
+                <span *ngIf="searchedTreasure()" class="center">Found treasure: {{ searchedTreasure()?.name }}</span>
             </div>
-            <main>
-                <section class="treasures-container" *ngIf="vm.treasures; let treasures">
-                    <treasure *ngFor="let treasure of treasures" [treasure]="treasure"/>
-                </section>
-            </main>
-        </ng-container>
+            <section class="treasures-container">
+                <treasure *ngFor="let treasure of foundTreasures()" [treasure]="treasure" />
+            </section>
+        </main>
     `,
 })
 export class TreasureListComponent {
-    private streaming = new BehaviorSubject(false);
-    streaming$ = this.streaming.asObservable();
+    searchId = model<number | null>(null);
+    streaming = signal(false);
+    streaming$ = toObservable(this.streaming);
 
     private getTreasure = new Subject<void>();
     private gettingTreasure$ = this.getTreasure.pipe(throttleTime(1000));
-    private findingTreasures$ = this.streaming.pipe(
+    private findingTreasures$ = this.streaming$.pipe(
       switchMap(auto => auto ? interval(1000) : EMPTY),
     );
 
-    foundTreasures$ = merge(this.gettingTreasure$, this.findingTreasures$).pipe(
-      scan((acc) => acc + 1, -1),
-      map((index) => treasuresMock.slice(0, index + 1)),
-    );
+    searchedTreasure = toSignal(toObservable(this.searchId).pipe(
+      switchMap(id => id === null ? EMPTY : this.getTreasureById(id))
+    ))
 
-    vm$ = combineLatest({
-        treasures: this.foundTreasures$.pipe(startWith([])),
-        streaming: this.streaming$.pipe(startWith(false)),
-    });
+    foundTreasures = toSignal(
+        merge(this.gettingTreasure$, this.findingTreasures$).pipe(
+            scan((acc) => acc + 1, -1),
+            tap(console.log),
+            map((index) => treasuresMock.slice(0, index + 1)),
+        )
+    );
 
     getNextArticle() {
         this.getTreasure.next();
     }
 
     toggleStreaming() {
-        this.streaming.next(!this.streaming.value);
+        this.streaming.update(streaming => !streaming);
     }
 
     getTreasureById(id: number) {
